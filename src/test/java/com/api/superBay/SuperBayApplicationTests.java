175a2e32febe34ad2be93d36ab43dc83c4a9776a
Guilherme <guiborgesTi>
2019-02-17 19:50:55 -0300

    estrutura do projeto

---
+++ src/test/java/com/api/superBay/SuperBayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.api.superBay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuperBayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
