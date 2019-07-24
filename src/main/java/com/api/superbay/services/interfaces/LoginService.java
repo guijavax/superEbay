7c697b98d6a979d0f0f8a599cae77771874a01df
Guilherme Carneiro Borges <gcarneib@everis.com>
2019-07-24 18:24:47 -0300

    inclusao do javadoc

--- src/main/java/com/api/superbay/services/interfaces/LoginService.java
+++ src/main/java/com/api/superbay/services/interfaces/LoginService.java
@@ -4,6 +4,18 @@ import org.springframework.http.ResponseEntity;
 
 import com.api.superbay.entities.LoginEntitie;
 
+/**
+ * 
+ * @author gcarneib<br>
+ * Classe responsável por efetuar o login
+ */
 public interface LoginService {
+	
+	/**
+	 * 
+	 * @param entitie
+	 * @return
+	 * metódo responsável por salvar o histórico de login
+	 */
 	ResponseEntity login(LoginEntitie entitie);
 }
