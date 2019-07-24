ad0b09ad24aa7f7ce06c85ec9d5b04e7e4b9235d
Guilherme Carneiro Borges <gcarneib@everis.com>
2019-07-24 16:01:54 -0300

    criacao do serviçoi pt2

--- src/main/java/com/api/superbay/entities/LoginEntitie.java
+++ src/main/java/com/api/superbay/entities/LoginEntitie.java
@@ -1,8 +1,46 @@
 package com.api.superbay.entities;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
-//@Entity
+@Entity(name = "login")
 public class LoginEntitie {
+	
+	@Id
+	@NotNull
+	private Long idLogin;
+	
+	@Column(name="id_user")
+	private Long idUser;
+	
+	@Column(name="data_login")
+	private String dataLogin;
+	
+	public Long getIdLogin() {
+		return idLogin;
+	}
 
+	public void setIdLogin(Long idLogin) {
+		this.idLogin = idLogin;
+	}
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getDataLogin() {
+		return dataLogin;
+	}
+
+	public void setDataLogin(String dataLogin) {
+		this.dataLogin = dataLogin;
+	}
 }
